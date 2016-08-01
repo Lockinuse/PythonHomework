7c8048815f9f6df6c42f16997293ea6606245de4
Breanna Plimpton <lockinuse@gmail.com>
2016-08-01 10:34:08 -0500

    Reviewing 5

--- Another Review for Final.py
+++ Another Review for Final.py
@@ -1,6 +1,4 @@
-place_holder = "The small brown fox jumps over the fence"
-print(place_holder)
-newlist = place_holder.split() # split method converts a string to a list. It is doing so by finding all the spaces
-newstring = newlist[2:4]
-newstring = ' '.join(newlist[2:4])
-print(newstring)
+Name = input("Please enter a name ")
+Age = input("Please enter your age ")
+People = {Name : Age}
+print(People)
