cf5e7ce8d9192e9e8faac5b45702c2fca9cfa75f
Breanna Plimpton <lockinuse@gmail.com>
2016-08-01 09:31:07 -0500

    Reviewing 2

--- BeforeFinalTesting.py
+++ BeforeFinalTesting.py
@@ -1,5 +1,10 @@
 list1 = [1,2,3,4,5,6,7,8,9,10]
 print(list1)
 for num in list1:
-    new = num * 4
-    print(new)
+    new = num * 3
+    if new % 2 ==0:
+        print(new)
+    else:
+        print("No")
+
+
