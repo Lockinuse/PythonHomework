552361efbbdd0687ada2657a0a6d5a9b7956370f
Breanna Plimpton <lockinuse@gmail.com>
2016-08-01 09:47:23 -0500

    Reviewing 3

--- BeforeFinalTesting.py
+++ BeforeFinalTesting.py
@@ -1,10 +1,23 @@
-list1 = [1,2,3,4,5,6,7,8,9,10]
-print(list1)
-for num in list1:
-    new = num * 3
-    if new % 2 ==0:
-        print(new)
-    else:
-        print("No")
+class Dog:
 
+    def __init__(self, Breed, Name, Age):
+        self.Breed = Breed
+        self.Name = Name
+        self.Age = Age
+
+    def get_Breed(self):
+        return self.Breed
+
+    def get_Name(self):
+        return self.Name
+
+    def get_Age(self):
+        return self.Age
+
+Breed = input("What breed if your dog: ")
+Name = input("What is your dogs name: " )
+Age = input("How old is your dog: ")
+
+dog1 = Dog(Breed, Name, Age)
+print("You have entered", (dog1.get_Name()))# You don't use self outside the class. dog1 is self
 
