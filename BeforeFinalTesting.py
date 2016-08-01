70ccac7d88f26fbdce6864f7fc8df2b2806bdcfa
Breanna Plimpton <lockinuse@gmail.com>
2016-08-01 10:17:16 -0500

    Reviewing 4

--- BeforeFinalTesting.py
+++ BeforeFinalTesting.py
@@ -14,10 +14,3 @@ class Dog:
     def get_Age(self):
         return self.Age
 
-Breed = input("What breed if your dog: ")
-Name = input("What is your dogs name: " )
-Age = input("How old is your dog: ")
-
-dog1 = Dog(Breed, Name, Age)
-print("You have entered", (dog1.get_Name()))# You don't use self outside the class. dog1 is self
-
